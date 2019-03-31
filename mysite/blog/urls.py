2ffbb686b126fa52da60b5ae7e7ccc21f21c8dda
davidzhu <hideonc@outlook.com>
2019-04-01 00:09:43 +0800

    创建订阅功能

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,7 +4,7 @@ __author__ = 'Boaz'
 
 from django.urls import path
 from . import views
-
+from .feed import LatestPostFeed
 
 app_name = 'blog'
 urlpatterns = [
@@ -15,4 +15,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('tag/<slug:tag_slug>', views.post_list, name="post_list_by_tag"),
+    path('feed/', LatestPostFeed(), name='post_feed'),
 ]
