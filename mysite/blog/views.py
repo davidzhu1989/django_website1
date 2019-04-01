6c1e6b07bf9aa6feaec3a4ee04c89c280186b6ed
davidzhu <hideonc@outlook.com>
2019-04-01 23:50:33 +0800

    搜索权重

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
@@ -96,10 +96,22 @@ def post_search(request):
             query = form.cleaned_data['query']
             # results = Post.objects.annotate(
             #     search=SearchVector('title', 'slug','body'),).filter(search=query)
-            search_vector = SearchVector('title', 'body')
+            # search_vector = SearchVector('title', 'body')
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
             search_query = SearchQuery(query)
-            results = Post.objects.annotate(search=search_vector,
-                    rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by('-rank')
+
+            # results = Post.objects.annotate(search=search_vector,
+            #         rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by('-rank')
+
+            # 默认权重D, C, B, A,分别对应 0.1， 0.2， 0.4， 1
+            results = Post.objects.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__get=0.3).order_by('-rank')
+
+            # results = Post.objects.annotate(
+            #     similarity=TrigramSimilarity('title', query),
+            # ).filter(similarity__gte=0.1).order_by('-similarity')
     return render(request, 'blog/post/search.html',
                   {'query': query, 'form': form, 'results': results})
 
